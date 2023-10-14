6047196a67ae29892963e4d28483e6b00627c7ba
Saad Tariq <saadtariq.dev@outlook.com>
2023-10-14 15:05:52 -0400

    add theme provider

--- src/components/providers/next-ui-provider.tsx
+++ src/components/providers/next-ui-provider.tsx
@@ -1,7 +1,18 @@
 "use client"
 
 import { NextUIProvider as Provider } from "@nextui-org/react"
+import { ThemeProvider } from "next-themes"
 
 export function NextUIProvider({ children }: { children: React.ReactNode }) {
-  return <Provider>{children}</Provider>
+  return (
+    <Provider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem={true}
+      >
+        {children}
+      </ThemeProvider>
+    </Provider>
+  )
 }
