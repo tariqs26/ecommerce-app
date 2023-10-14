94c8f736869b7a5595289ec438c1fa4f2c738f42
Saad Tariq <saadtariq.dev@outlook.com>
2023-10-14 15:06:31 -0400

    add main content styling, and footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import { Inter } from "next/font/google"
 import type { Metadata } from "next"
+import { siteConfig } from "@/config/site"
 import { NextUIProvider } from "@/components/providers/next-ui-provider"
 import "./globals.css"
+import { Footer } from "@/components/Footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata: Metadata = {
-  title: "Ecommerce",
-}
+export const metadata: Metadata = siteConfig
 
 export default function RootLayout({
   children,
@@ -17,7 +17,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextUIProvider>{children}</NextUIProvider>
+        <NextUIProvider>
+          <div className="flex flex-col min-h-screen">
+            <main className="p-4 max-w-5xl mx-auto min-w-[300px] flex-1 w-full">
+              {children}
+            </main>
+            <Footer />
+          </div>
+        </NextUIProvider>
       </body>
     </html>
   )
