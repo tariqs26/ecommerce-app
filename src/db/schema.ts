62e615ca613bb3b91d4de7d459f0954fe449ee89
Saad Tariq <saadtariq.dev@outlook.com>
2023-10-13 22:10:07 -0400

    create schema file ,with user

---
+++ src/db/schema.ts
@@ -0,0 +1,7 @@
+import { serial, text, pgTable } from "drizzle-orm/pg-core"
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+})
