81da1f68f5ff5cd7536cb9c3eae9778207ca8cf0
Saad Tariq <saadtariq.dev@outlook.com>
2023-10-13 22:09:50 -0400

    add db connection client

---
+++ src/db/index.ts
@@ -0,0 +1,7 @@
+import postgres from "postgres"
+import { drizzle } from "drizzle-orm/postgres-js"
+import * as schema from "./schema"
+
+const queryClient = postgres(process.env.DATABASE_URL)
+
+export const db = drizzle(queryClient, { schema })
