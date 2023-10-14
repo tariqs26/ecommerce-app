4ca438228eb1f74d17ad7bce99866de60b3b07a5
Saad Tariq <saadtariq.dev@outlook.com>
2023-10-14 15:05:57 -0400

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { siteConfig } from "@/config/site"
+
+export function Footer() {
+  return (
+    <footer className="text-center text-sm text-default-500">
+      <p>
+        &copy; {new Date().getFullYear()} {siteConfig.title}
+      </p>
+    </footer>
+  )
+}
