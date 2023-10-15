89dbacd30d697b96e57d0854901600459c27b8b8
Saad Tariq <saadtariq.dev@outlook.com>
2023-10-15 00:19:59 -0400

    use custom spinner component

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 // @ts-ignore
-import {experimental_useFormStatus as useFormStatus } from "react-dom"
+import { experimental_useFormStatus as useFormStatus } from "react-dom"
 import { Button, ButtonProps } from "@nextui-org/react"
+import {Spinner} from "./Spinner"
 
 export function SubmitButton(props: ButtonProps) {
   const { pending } = useFormStatus()
@@ -12,28 +13,7 @@ export function SubmitButton(props: ButtonProps) {
       type="submit"
       color="warning"
       isLoading={pending}
-      spinner={
-        <svg
-          className="animate-spin h-5 w-5 text-current"
-          fill="none"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          />
-          <path
-            className="opacity-75"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            fill="currentColor"
-          />
-        </svg>
-      }
+      spinner={<Spinner />}
       {...props}
     />
   )
