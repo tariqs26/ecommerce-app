de7db9a7e9bcdbbf82fec90c843fcfe18329648d
Saad Tariq <saadtariq.dev@outlook.com>
2023-10-14 22:27:28 -0400

    add custom not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-4">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="text-xl">Page not found</p>
+    </div>
+  )
+}
