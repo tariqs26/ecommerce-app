8a6e7488a356c10dd6082cd26e7e23d577a70eac
Saad Tariq <saadtariq.dev@outlook.com>
2023-10-15 01:01:17 -0400

    update footer with placeholder content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,23 @@
-import { siteConfig } from "@/config/site"
+import { footerConfig } from "@/config/footer"
 
 export function Footer() {
   return (
-    <footer className="text-center text-sm text-default-500 p-2">
-      <p>
-        &copy; {new Date().getFullYear()} {siteConfig.title}
-      </p>
+    <footer className="bg-primary-900 p-10 ">
+      <div className="m-auto max-w-7xl xl:grid-cols-3 grid md:grid-cols-2 grid-cols-1 gap-8">
+        {footerConfig.map((section) => (
+          <div className="flex flex-col" key={section.title}>
+            <span className="font-semibold text-lg mb-3 text-warning">{section.title}</span>
+            {section.links.map((link) => (
+              <a
+                key={link}
+                className="hover:underline hover:cursor-pointer text-primary-foreground/70 mb-1"
+              >
+                {link}
+              </a>
+            ))}
+          </div>
+        ))}
+      </div>
     </footer>
   )
 }
---
+++ src/config/footer.ts
@@ -0,0 +1,14 @@
+export const footerConfig = [
+  {
+    title: "Services",
+    links: ["Branding", "Design", "Marketing", "Advertisement"],
+  },
+  {
+    title: "Company",
+    links: ["About us", "Contact", "Jobs", "Press kit"],
+  },
+  {
+    title: "Legal",
+    links: ["Terms of use", "Privacy policy", "Cookie policy"],
+  },
+]
