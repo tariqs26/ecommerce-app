97e2b2c0f82db0eae2b76425fc1893663cbd6385
Saad Tariq <saadtariq.dev@outlook.com>
2023-10-15 01:34:21 -0400

    use client for migrations

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,13 +1,15 @@
-import { Pool } from "pg"
+import { Client } from "pg"
 import { migrate } from "drizzle-orm/node-postgres/migrator"
 import { drizzle } from "drizzle-orm/node-postgres"
 import "dotenv/config"
 
-const client = new Pool({
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
 })
 
 async function main() {
+  console.log("Connecting to database...")
+  await client.connect()
   console.log("Running migrations...")
   await migrate(drizzle(client), {
     migrationsFolder: "drizzle",
